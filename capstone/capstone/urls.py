b60ed2ba64f18f44a0d31d599833f88f742edc40
unknown <richmarti.766@gmail.com>
2018-05-04 19:20:10 -0500

    missing url

--- capstone/capstone/urls.py
+++ capstone/capstone/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('courses/', include('courses.urls')),
     path('best_path/', include('best_path.urls')),
     path('opath/', include("opath.urls"))
 ]
