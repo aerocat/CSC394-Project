51472a825fe0e32736c8c0480d57c969dd72d9a5
kamilzwierz <kamilzwierzchlejski@gmail.com>
2018-05-09 19:47:40 -0500

    Fix?

--- capstone/LandingPage/urls.py
+++ capstone/LandingPage/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
 	path('<int:student_id_in>/student', views.StudentSingleView, name='single_student'),
 	path('faculty/', views.FacultyView.as_view(), name='all_faculty'),
 	path('<int:faculty_id_in>/faculty', views.FacultySingleView, name='single_faculty'),
-    path('courses/', views.CourseListView.as_view(), name='course-list'),
-    path('<str:class_code_in>/course', views.CourseSingleView, name='single_course'),
 ]
