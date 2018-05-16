3ddd9a435165732d76e69569730a3230d1ddf77c
aerocat <mferri1@depaul.edu>
2018-05-16 14:02:49 -0500

    updated accounts

--- capstone/accounts/models.py
+++ capstone/accounts/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
-class Faculty(models.Model):
-	faculty_id = models.IntegerField()
-	faculty_name_first = models.CharField(max_length=50)
-	faculty_name_last = models.CharField(max_length=50)
-	faculty_email = models.CharField(max_length=50)
+# Extension of the User Object
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	student_id = models.IntegerField(default=0000)
+	GPA = models.FloatField(default=0.00)
+	# more fields will go here
 	def __str__(self):
-		return self.faculty_name_last
+		return self.user.username
 
-class Student(models.Model):
-	student_id = models.IntegerField()
-	student_name_first = models.CharField(max_length=50)
-	student_name_last = models.CharField(max_length=50)
-	student_email = models.CharField(max_length=50)
-	def __str__(self):
-		return self.student_name_last
+def create_profile(sender, **kwargs):
+	if kwargs['created']:
+		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
