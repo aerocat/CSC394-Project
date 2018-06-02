715e2ac5503fbca73860aef8e1e4a31d57cf73d6
kamilzwierz <kamilzwierzchlejski@gmail.com>
2018-06-02 17:03:51 -0500

    faculty and student single view fix

--- capstone/LandingPage/views.py
+++ capstone/LandingPage/views.py
@@ -8,25 +8,19 @@ from accounts.models import Faculty, Student
 def index(request):
 	return render(request, 'LandingPage/index.html')
 
-class FacultyView(generic.ListView):
-	model = Faculty
-	context_object_name = 'faculty_list'
-	template_name = 'LandingPage/faculty.html'
-
-class StudentView(generic.ListView):
-	model = Student
-	context_object_name = 'student_list'
-	template_name = 'LandingPage/students.html'
-
 def StudentSingleView(request, student_id_in):
-	student = Student.objects.get(student_id=student_id_in)
-
-	return HttpResponse("View for student with id %s." % student.student_id)
+	try:
+		student = Student.objects.get(student_id=student_id_in)
+	except:
+		return render(request, 'LandingPage/missing_student.html')
+	return render(request, 'LandingPage/single_student.html', {'student': student})
 
 def FacultySingleView(request, faculty_id_in):
-	faculty = Faculty.objects.get(faculty_id=faculty_id_in)
-
-	return HttpResponse("View for faculty with id %s." % faculty.faculty_id)
+	try:
+		faculty = Faculty.objects.get(faculty_id=faculty_id_in)
+	except:
+		return render(request, 'LandingPage/missing_faculty.html')
+	return render(request, 'LandingPage/single_faculty.html', {'faculty': faculty})
 
 def FacultyListView(request, ):
     faculty_list = []
