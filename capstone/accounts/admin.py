e045f0ab473d790e907dd7abb8d598acbf6e3ed4
bternes351 <bternes351@gmail.com>
2018-06-05 09:29:51 -0500

    User Extension
    -Student is now an extension of user
    -UserProfile removed
    -Student list displaying properly

--- capstone/accounts/admin.py
+++ capstone/accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserProfile
+from .models import Student
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(Student)
 # admin.site.register(Faculty)
--- capstone/accounts/models.py
+++ capstone/accounts/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Extension of the User Object
-class UserProfile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	student_id = models.IntegerField(default=0000)
-	GPA = models.FloatField(default=0.00)
-	# more fields will go here
-	def __str__(self):
-		return self.user.username
+##Extension of the User Object
+#lass UserProfile(models.Model):
+#	user = models.OneToOneField(User, on_delete=models.CASCADE)
+#	student_id = models.IntegerField(default=0000)
+#	GPA = models.FloatField(default=0.00)
+# more fields will go here
+#	def __str__(self):
+#		return self.user.username
 
 def create_profile(sender, **kwargs):
 	if kwargs['created']:
-		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+		user_profile = Student.objects.create(user=kwargs['instance'])
 
 post_save.connect(create_profile, sender=User)
 
@@ -27,9 +27,10 @@ class Faculty(models.Model):
 		return self.faculty_name_last
 
 class Student(models.Model):
-	student_id = models.IntegerField()
+	user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+	student_id = models.IntegerField(default=0000)
 	student_name_first = models.CharField(max_length=50)
 	student_name_last = models.CharField(max_length=50)
 	student_email = models.CharField(max_length=50)
 	def __str__(self):
-		return self.student_name_last
+		return self.user.username
